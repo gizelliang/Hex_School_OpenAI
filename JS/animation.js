cb756a535b345ba1c251a8a402b4ab1bd077ffbf
Gizel Liang <gizelliang@gmail.com>
2023-06-07 09:31:25 +0800

    Jquery installed for the dropdown menu

--- JS/animation.js
+++ JS/animation.js
@@ -1,24 +1,23 @@
-$(document).ready(function(event){
-    
-// 開起選單
+＄(document).ready(function(){
+    // 開起選單
 $('.dropdown-btn').on("click",function(e) {
-  $('.dropdown-menu').toggleClass('show');
-});
-
-// 切換按鈕文字
-$('.new-to-old').on("click",function(e) {
-  e.preventDefault();
-  $('.dropdown-menu').toggleClass('show');
- $('.dropdown-btnText').text($('.new-to-old').text());
-});
-
-$('.old-to-new').on("click",function(e) {
-  e.preventDefault();
-  $('.dropdown-menu').toggleClass('show');
-  $('.dropdown-btnText').text($('.old-to-new').text());
-});
-
-// .text() 運用：https://www.w3schools.com/jquery/html_text.asps
-
+    $('.dropdown-menu').toggleClass('show');
+  });
+  
+  // 切換按鈕文字
+  $('.new-to-old').on("click",function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+   $('.dropdown-btnText').text($('.new-to-old').text());
+  });
+  
+  $('.old-to-new').click(function(e) {
+    e.preventDefault();
+    $('.dropdown-menu').toggleClass('show');
+    $('.dropdown-btnText').text($('.old-to-new').text());
+  });
+  
+  // .text() 運用：https://www.w3schools.com/jquery/html_text.asp
 
 })
+
